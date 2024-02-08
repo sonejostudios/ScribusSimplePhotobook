5db267a5a15ff9985a496d68b85a49015c77af2b
Sonejo Studios <azvinz@yahoo.de>
2024-02-08 12:24:24 +0100

    Updated to python3 for Scribus >= 1.6.0

--- auto_photobook_python2.py
+++ auto_photobook_python2.py
@@ -7,9 +7,9 @@ import os
 
 try:
     import scribus
-except:
-    print("This Python script is written for the Scribus scripting interface.")
-    print("It can only be run from within Scribus.")
+except ImportError, err:
+    print "This Python script is written for the Scribus scripting interface."
+    print "It can only be run from within Scribus."
     sys.exit(1)
 
 
